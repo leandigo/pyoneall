b25d19c5ac422dcffcd6d78ba1a73560f99f5613
Ekevoo <x@ekevoo.com>
2015-09-13 23:11:22 -0400

    Stubbed out a test suite.

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-import os
+# -*- coding: utf-8 -*-
+from os.path import join, dirname
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
-README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+README = open(join(dirname(__file__), 'README.rst')).read()
 setup(
     name='pyoneall',
     version='0.1.1',
@@ -10,6 +11,7 @@ setup(
     license='MIT License, see LICENSE file',
     description='OneAll API wrapper (http://www.oneall.com). Provides unified API for 30+ social networks',
     long_description=README,
+    test_suite='tests',
     url='http://www.leandigo.com/',
     author='Michael Greenberg / Leandigo',
     author_email='michael@leandigo.com',
---
+++ tests/test_auth.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from unittest import TestCase
+
+
+class TestOneAll(TestCase):
+    def test_whether_test_runs(self):
+        self.assertTrue(True)
